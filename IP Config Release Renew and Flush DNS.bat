5e31336edfe0d283d6123006061ca9902da203ff
FunkyJustin <sabiojustin10@gmail.com>
2024-06-30 13:20:29 +0800

    Add files via upload

--- IP Config Release Renew and Flush DNS.bat
+++ IP Config Release Renew and Flush DNS.bat
@@ -1,5 +1,6 @@
 @echo off
 
+:begin
 echo Releasing IP Address...
 ipconfig /release
 
@@ -13,4 +14,16 @@ ipconfig /flushdns
 
 echo.
 
-pause
+:ask
+set /p choice=Do you want to run the program again? (yes/no) or (y/n):
+
+if /i "%choice%"=="yes" goto begin
+if /i "%choice%"=="y" goto begin
+if /i "%choice%"=="no" goto end
+if /i "%choice%"=="n" goto end
+
+echo Invalid choice. Please type (yes/no) or (y/n).
+goto ask
+
+:end
+echo Program ended.
