9336570b6bb345202185e298e8ec3b63867cac77
FunkyJustin <sabiojustin10@gmail.com>
2024-12-06 01:20:24 +0800

    Update disable_sleep_hibernate.bat

--- disable_sleep_hibernate.bat
+++ disable_sleep_hibernate.bat
@@ -1,4 +1,14 @@
 @echo off
+
+:: Check if the script is running with administrative privileges
+:: If not, it will re-run itself with elevated privileges
+net session >nul 2>&1
+if %errorLevel% neq 0 (
+    echo Requesting administrative privileges...
+    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    exit
+)
+
 echo Disabling sleep and hibernation...
 
 :: Disable sleep
